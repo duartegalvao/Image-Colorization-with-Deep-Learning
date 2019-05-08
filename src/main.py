d9bfee021cf22b0acae374c9a9a6318ee79bc31e
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2019-05-08 16:14:38 +0200

    main

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import tensorflow as tf
+
+from model import Model
+
+from helpers import *
+
+SEED = 24
+
+X_train, Y_train, X_val, Y_val, X_test, Y_test = load_CIFAR(SEED)
+
+np.random.seed(SEED)
+tf.random.set_random_seed(SEED)
+
+
+with tf.Session() as sess:
+    
+    MLP = Model(sess, SEED)
+    
+    MLP.compile()
+    
+    MLP.train(X_train, Y_train, X_val, Y_val)
+    
+    print(MLP.evaluate(X_train, Y_train))
+    print(MLP.evaluate(X_val, Y_val))
+    print(MLP.evaluate(X_test, Y_test))
+    
