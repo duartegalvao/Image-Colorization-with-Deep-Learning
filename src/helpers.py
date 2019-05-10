0f2620f048457719be888d347c20b49b79ffb7b9
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2019-05-10 12:33:19 +0200

    Save preprocd dataset

--- src/helpers.py
+++ src/helpers.py
@@ -3,8 +3,13 @@ import os
 import numpy as np
 from six.moves import cPickle as pickle
 from skimage import color, io
+from pathlib import Path
 
-def load_batch(file_path):
+
+DATA_PATH = Path("data")
+
+
+def _load_batch(file_path):
     """
         Loads data given path to file.
 
@@ -16,19 +21,43 @@ def load_batch(file_path):
             - Y: One hot encoding.
             - y: labels.
     """
-    path = "data/" + file_path
-
-    with open(path, 'rb') as f:
+    with open(DATA_PATH / file_path, 'rb') as f:
         data = pickle.load(f, encoding='bytes')
         X = data[b'data']
     return X
 
 
-def load_CIFAR(seed):
-    data = load_batch('data_batch_1')
+def _is_CIFAR_preprocd(seed):
+    return (DATA_PATH / "preproc_{}.npz".format(seed)).exists()
+
+
+def _load_preprocd_CIFAR(seed):
+    with np.load(DATA_PATH / "preproc_{}.npz".format(seed)) as data:
+        X_train = data['X_train']
+        Y_train = data['Y_train']
+        X_val = data['X_val']
+        Y_val = data['Y_val']
+        X_test = data['X_test']
+        Y_test = data['Y_test']
+    return X_train, Y_train, X_val, Y_val, X_test, Y_test
+
+
+def _save_preprocd_CIFAR(cifar, seed):
+    X_train, Y_train, X_val, Y_val, X_test, Y_test = cifar
+    np.savez(DATA_PATH / "preproc_{}.npz".format(seed),
+             X_train=X_train,
+             Y_train=Y_train,
+             X_val=X_val,
+             Y_val=Y_val,
+             X_test=X_test,
+             Y_test=Y_test)
+
+
+def _preproc_CIFAR(seed):
+    data = _load_batch('data_batch_1')
 
     for i in range(4):
-        data_batch = load_batch('data_batch_{0}'.format(i+2))
+        data_batch = _load_batch('data_batch_{0}'.format(i + 2))
 
         data = np.vstack((data, data_batch))
     
@@ -39,7 +68,7 @@ def load_CIFAR(seed):
 
     data_train = data[5000:, :]
     data_val = data[:5000, :]
-    data_test = load_batch('test_batch')
+    data_test = _load_batch('test_batch')
 
     data_train = data_train.reshape((45000, 3, 32, 32)).transpose(0, 2, 3, 1)
     data_val = data_val.reshape((5000, 3, 32, 32)).transpose(0, 2, 3, 1)
@@ -50,6 +79,17 @@ def load_CIFAR(seed):
     X_test, Y_test = split_luminosity_and_lab(data_test)
 
     return X_train, Y_train, X_val, Y_val, X_test, Y_test
+
+
+def load_CIFAR(seed):
+    if _is_CIFAR_preprocd(seed):
+        print("Found preprocessed data for seed={}.".format(seed))
+        return _load_preprocd_CIFAR(seed)
+    else:
+        print("Found no preprocessed data. Pre-processing for seed={}.".format(seed))
+        cifar = _preproc_CIFAR(seed)
+        _save_preprocd_CIFAR(cifar, seed)
+        return cifar
 
 
 def split_luminosity_and_lab(rgb_images):
